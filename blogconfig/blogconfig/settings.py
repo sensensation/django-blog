f6f47d684969515aea8df37e595e9ba80d137f33
cybersen <gashnya_official@bk.ru>
2022-10-13 17:33:45 +0300

    Ver 4 - Signup ang Login update

--- blogconfig/blogconfig/settings.py
+++ blogconfig/blogconfig/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = "/static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- blogconfig/blogconfig/urls.py
+++ blogconfig/blogconfig/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("bboard/", include('bboard.urls')),
     path("accounts/", include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    
 ]
