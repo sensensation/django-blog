9ef5e0d0d1aa4fdefce0b1de1290c2681049e106
cybersen <gashnya_official@bk.ru>
2022-10-13 16:55:38 +0300

    Ver 3 - Log in update

--- blogconfig/bboard/views.py
+++ blogconfig/bboard/views.py
@@ -15,6 +15,13 @@ class BbCreateView(CreateView):
         context['rubrics'] = Rubric.objects.all()
         return context
 
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {"bbs": bbs, "rubrics": rubrics, "current_rubric": current_rubric}
+    return render(request, "bboard/by_rubric.html", context)
+
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
@@ -46,10 +53,5 @@ class Post():
         return redirect('index')
 
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {"bbs": bbs, "rubrics": rubrics, "current_rubric": current_rubric}
-    return render(request, "bboard/by_rubric.html", context)
+
 
