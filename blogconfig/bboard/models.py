1766edca65d1a62fcdc14a9935bf0ef7c58fd58c
cybersen <gashnya_official@bk.ru>
2022-10-20 10:59:46 +0300

    Ver 5d - regular update

--- blogconfig/bboard/models.py
+++ blogconfig/bboard/models.py
@@ -6,14 +6,13 @@ class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name="Заголовок")
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
-    image = models.ImageField(upload_to='images', null=True, blank=True, verbose_name="Картинка") 
+    image = models.ImageField(upload_to='images', null=True, blank=True, verbose_name="Картинка", default='images/default_pic.jpg') 
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
 
     class Meta:
         verbose_name_plural = "Посты"
         verbose_name = "Пост"
         ordering = ["-published"]
-
 
 class Rubric(models.Model):
     name = models.CharField(max_length=20, verbose_name="Название", db_index=True)
