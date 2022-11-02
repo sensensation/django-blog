391549f07941c09d5df6e3d0e13f0f58d62c2167
cybersen <gashnya_official@bk.ru>
2022-11-02 12:45:24 +0300

    Ver 6h - CSS rubric update

--- blogconfig/blogconfig/wsgi.py
+++ blogconfig/blogconfig/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogconfig.settings")
 
 application = get_wsgi_application()
+
+#для синхронного питона
