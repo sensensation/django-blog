1c4503bbd5e0fb89111a3bbaefaec243803fc545
cybersen <gashnya_official@bk.ru>
2022-11-04 00:16:12 +0300

    Ver 7b - post delete bug fix

--- blogconfig/bboard/views.py
+++ blogconfig/bboard/views.py
@@ -63,15 +63,14 @@ class Post:
     @login_required(login_url="login")
     @staticmethod
     def post_delete(request, pk):
-         post_to_delete=Bb.objects.get(id=pk)
-         post_to_delete.delete()
-         url = reverse_lazy('index')
-         return HttpResponseRedirect(url)
+        #  post_to_delete=Bb.objects.get(id=pk)
+        #  post_to_delete.delete()
+        #  url = reverse_lazy('index')
+        #  return HttpResponseRedirect(url)
 
-          # post = Bb.objects.get(id=pk)
-        # print(Bb.objects.get(id=pk))
-        # if request.method == "POST":
-        #     post.delete()
-        #     return redirect("index")
-        # return redirect("index")
+        post = Bb.objects.get(id=pk)
+        if request.method == "POST":
+            post.delete()
+            return redirect("index")
+        return redirect("index")
        
